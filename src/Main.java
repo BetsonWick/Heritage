efa84dd26fe9309c10009828424303880a898778
BetsonWick <strelnikovila@gmail.com>
2020-02-20 22:23:48 +0300

    Idea setup

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import org.newdawn.slick.*;
+
+public class Main extends BasicGame {
+
+    public Main(String title) {
+        super(title);
+    }
+
+    public static void main(String[] args) throws SlickException {
+        AppGameContainer container = new AppGameContainer(new Main("Heritage"));
+
+    }
+
+    @Override
+    public void init(GameContainer container) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer container, int delta) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer container, Graphics g) throws SlickException {
+
+    }
+}
